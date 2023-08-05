bda16e0fcd4a2fbb103415fa1feaff9cf30d0918
paytonwu90 <sw9008k@gmail.com>
2023-08-06 03:55:32 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    // 啟動 server 時預設開啟的頁面
+    open: 'index.html',
+  },
+})
