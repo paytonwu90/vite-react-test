f8e36e9b16768522cbdf89be481a4a734e9b704b
paytonwu90 <sw9008k@gmail.com>
2023-08-06 17:27:06 +0800

    set the correct base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/vite-react-test/' : '/',
   plugins: [react()],
   server: {
     // 啟動 server 時預設開啟的頁面
